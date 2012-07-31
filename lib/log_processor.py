467150cc7b139346d811a769ad11e909fe4f6379
Max Kalika <max@topsy.com>
2012-07-31 09:49:18 -0700

    Enhance log_processor module to support a reset trigger and a starting value.  CONFIG CHANGE NOTE: what used to be regex is now called count.

--- lib/log_processor.py
+++ lib/log_processor.py
@@ -4,16 +4,25 @@ import snmpy
 class log_processor(snmpy.plugin):
     def create(self):
         for k, v in sorted(self.conf['objects'].items()):
+            extra = {
+                'count': re.compile(v['count']),
+                'reset': re.compile(v['reset']) if 'reset' in v else None,
+                'start': int(v['start']) if 'start' in v else 0,
+            }
+
             self.data['1.%s' % k] = 'string', v['label']
-            self.data['2.%s' % k] = 'integer', 0, {'re': re.compile(v['regex'])}
+            self.data['2.%s' % k] = 'integer', extra['start'], extra
 
         self.tail()
 
     @snmpy.plugin.task
     def tail(self):
-        for line in snmpy.plugin.tail(self.conf['logfile']):
+        for line in snmpy.plugin.tail(self.conf['file_name']):
             for item in self.data['2.0':]:
-                find = self.data[item:'re'].search(line)
-                if find:
-                    self.data[item] = self.data[item:True] + (int(find.group(1)) if len(find.groups()) > 0 else 1)
+                count = self.data[item:'count'].search(line)
+                if count:
+                    self.data[item] = self.data[item:True] + (int(count.group(1)) if len(count.groups()) > 0 else 1)
+                    break
+                if self.data[item:'reset'] is not None and self.data[item:'reset'].search(line):
+                    self.data[item] = self.data[item:'start']
                     break
