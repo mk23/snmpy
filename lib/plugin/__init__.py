051a2309a1a4e378a6328dc5047f3704ccf43783
Max Kalika <max@topsy.com>
2013-09-15 17:41:28 -0700

    Issue #12: port periodic_cmd module to agentx-compatible snmpy.

--- lib/plugin/__init__.py
+++ lib/plugin/__init__.py
@@ -2,3 +2,7 @@ class Plugin(object):
     def __init__(self, conf):
         self.conf = conf
         self.name = conf['name']
+        self.update()
+
+    def update(self):
+        raise(NotImplementedError('plugin module is missing update() method'))
--- lib/plugin/periodic_cmd.py
+++ lib/plugin/periodic_cmd.py
@@ -1,4 +1,21 @@
+import __builtin__
+import re
 import snmpy
+import subprocess
 
 class periodic_cmd(snmpy.Plugin):
-    pass
+    def update(self):
+        text = subprocess.Popen(self.conf['exec'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
+        print text
+        for indx in xrange(len(self.conf['items'])):
+            item, conf = self.conf['items'][indx].items().pop()
+            regex = re.compile(conf['regex'])
+            found = regex.findall(text)
+
+            if found:
+                if regex.groups == 0:
+                    self.conf['items'][indx][item]['val'] = len(found)
+                elif not conf.has_key('cdef'):
+                    self.conf['items'][indx][item]['val'] = found[0].strip()
+                else:
+                    self.conf['items'][indx][item]['val'] = getattr(__builtin__, conf['cdef'])([int(i) for i in found])
