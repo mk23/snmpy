4b5b85df20409d0358918a4f93838f6c98bf070c
Max Kalika <max@topsy.com>
2013-10-09 10:38:46 -0700

    Convert value_cmd to new plugin data model.

--- lib/plugin/value_cmd.py
+++ lib/plugin/value_cmd.py
@@ -12,13 +12,20 @@ class value_cmd(snmpy.plugin.ValuePlugin):
         'avg': lambda l: 0 if len(l) == 0 else sum(l) / len(l),
     }
 
+    def __init__(self, conf):
+        self.item_attributes = {
+            'cdef': None,
+            'join': '',
+        }
+        snmpy.plugin.ValuePlugin.__init__(self, conf)
+
     def update(self):
         text = subprocess.Popen(self.conf['object'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
         for item in self:
             find = re.findall(self[item].regex, text)
 
             if find:
-                if self[item:'cdef'] in self.CDEF_FUNCS:
+                if self[item].cdef in self.CDEF_FUNCS:
                     self[item] = self.CDEF_FUNCS[self[item].cdef](self[item].native(i) for i in find)
                 else:
-                    self[item] = self[item].native(self[item:'join':''].join(find))
+                    self[item] = self[item].native(self[item].join.join(find))
