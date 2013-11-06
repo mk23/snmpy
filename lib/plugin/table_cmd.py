f60ceb1319575e16f2ab88c69c15b69d0ca2b884
Max Kalika <max@topsy.com>
2013-11-06 03:59:05 -0800

    Issue #12: Add table cmd plugin module.

---
+++ lib/plugin/table_cmd.py
@@ -0,0 +1,11 @@
+import snmpy.parser
+import snmpy.plugin
+import subprocess
+
+class table_cmd(snmpy.plugin.TablePlugin):
+    def update(self):
+        self.clear()
+
+        text = subprocess.Popen(self.conf['object'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
+        for item in snmpy.parser.parse_table(self.conf['parser'], text):
+            self.append(item)
