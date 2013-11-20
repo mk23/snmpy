76fdabd725dfdf208725268dc92c5a5cc886d41a
Max Kalika <max@topsy.com>
2013-11-20 08:23:52 -0800

    Issue #12: Convert log_processor plugin module to agentx.

---
+++ lib/plugin/log_processor.py
@@ -0,0 +1,53 @@
+import logging
+import os
+import snmpy.parser
+import snmpy.plugin
+import time
+
+class log_processor(snmpy.plugin.ValuePlugin):
+    def __init__(self, conf):
+        snmpy.plugin.ValuePlugin.__init__(self, conf)
+
+        self.tail(self.conf['object'].format(**self.conf['snmpy_extra']))
+
+    def update(self):
+        pass
+
+    @snmpy.task_func(snmpy.THREAD_TASK)
+    def tail(self, name):
+        while True:
+            try:
+                data = open(name)
+                data.seek(0, 2) # start at the end
+                logging.debug('%s: opened file for tail', name)
+                break
+            except IOError as e:
+                logging.info('%s: cannot open for tail: %s', name, e)
+                time.sleep(5)
+            except Exception as e:
+                snmpy.log_error(e)
+
+        while True:
+            spot = data.tell()
+            stat = os.fstat(data.fileno())
+
+            if os.stat(name).st_ino != stat.st_ino or stat.st_nlink == 0 or spot > stat.st_size:
+                try:
+                    data = open(name)
+                    logging.info('%s: repopened for tail: moved, truncated, or removed', name)
+                except IOError as e:
+                    logging.info('%s: cannot open for tail: %s', name, e)
+            elif spot != stat.st_size:
+                text = data.read(stat.st_size - spot)
+                while True:
+                    indx = text.find('\n')
+                    if indx == -1:
+                        data.seek(-len(text), 1)
+                        break
+                    line = text[0:indx]
+                    text = text[indx + 1:]
+
+                    for item in self:
+                        self[item] = snmpy.parser.parse_value(line, self[item], ignore=True)
+
+            time.sleep(1)
