5189d1b2ceeacb7839c3b9088b161e0082ff4975
Max Kalika <max.kalika+projects@gmail.com>
2015-05-02 13:49:23 -0700

    Issue #53: Add optional md5 hash item in file_value module.

--- lib/module/file_value.py
+++ lib/module/file_value.py
@@ -1,3 +1,4 @@
+import hashlib
 import logging
 import os
 import snmpy.module
@@ -32,11 +33,22 @@ class file_value(snmpy.module.ValueModule):
                 {'file_uid':   {'type': 'integer', 'func': lambda x: x}},
                 {'file_gid':   {'type': 'integer', 'func': lambda x: x}},
             ] + conf.get('items', [])
+        if conf.get('use_hash'):
+            conf['items'].append({'file_md5': {'type': 'string'}})
+
 
         snmpy.module.ValueModule.__init__(self, conf)
 
+    def md5sum(self):
+        md5 = hashlib.md5()
+        with open(self.conf['object'], 'rb') as f:
+            for part in iter(lambda: f.read(1024 * md5.block_size), b''):
+                md5.update(part)
+
+        return md5.hexdigest()
+
     def update(self):
-        info = text = None
+        info = text = hash = None
 
         if self.conf.get('use_stat'):
             info = os.lstat(self.conf['object'])
@@ -44,9 +56,14 @@ class file_value(snmpy.module.ValueModule):
         if self.conf.get('use_text'):
             text = open(self.conf['object']).read()
             logging.debug('%s: read %d bytes', self.conf['object'], len(text))
+        if self.conf.get('use_hash'):
+            hash = self.md5sum()
+            logging.debug('%s: computed md5sum: %s', self.conf['object'], hash)
 
         for item in self:
             if hasattr(self[item], 'func') and info:
                 self[item] = self[item].func(getattr(info, 'st_%s' % item[5:], info.st_mode))
+            elif item == 'file_md5':
+                self[item] = hash
             elif text:
                 self[item] = snmpy.parser.parse_value(text, self[item])
