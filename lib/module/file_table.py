d0207ae21f05351a2dbec12406c9a836b96b277a
Max Kalika <max.kalika+projects@gmail.com>
2017-01-27 12:04:06 -0800

    Issue #67: Use command-line extra data to format object configuration in file_table module.

--- lib/module/file_table.py
+++ lib/module/file_table.py
@@ -4,6 +4,6 @@ import snmpy.parser
 
 class file_table(snmpy.module.TableModule):
     def update(self):
-        text = open(self.conf['object']).read()
+        text = open(self.conf['object'].format(**self.conf['snmpy_extra'])).read()
         for item in snmpy.parser.parse_table(self.conf['parser'], text):
             self.append(item)
