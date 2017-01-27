a588cac693d0dacaa95f598c7573a289c10f881a
Max Kalika <max.kalika+projects@gmail.com>
2017-01-27 12:03:59 -0800

    Issue #67: Use command-line extra data to format object configuration in exec_value module.

--- lib/module/exec_value.py
+++ lib/module/exec_value.py
@@ -12,6 +12,6 @@ class exec_value(snmpy.module.ValueModule):
         snmpy.module.ValueModule.__init__(self, conf)
 
     def update(self):
-        text = subprocess.Popen(self.conf['object'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
+        text = subprocess.Popen(self.conf['object'].format(**self.conf['snmpy_extra']), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
         for item in self:
             self[item] = snmpy.parser.parse_value(text, self[item])
